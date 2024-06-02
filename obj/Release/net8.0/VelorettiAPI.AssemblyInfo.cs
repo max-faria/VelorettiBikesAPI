319bf3d606c7b560c8c5c5c197027d03020236e9
Max Faria <127763619+max-faria@users.noreply.github.com>
2024-06-02 17:54:51 +1000

    fix: fix deploy

--- obj/Release/net8.0/VelorettiAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/VelorettiAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4952bfa515ca0de3ffbf6438c08917a686715904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef8aa76d08156a2d2c4aa048222b67f91b3a5fe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
