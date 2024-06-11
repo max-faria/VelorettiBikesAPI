b48ac3c9b7319a0bdc843ae1b5b842007b9117db
Max Faria <127763619+max-faria@users.noreply.github.com>
2024-06-11 21:17:55 +1000

    hardcoding email port

--- obj/Debug/net8.0/VelorettiAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/VelorettiAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10f03ba997671bb6841e53f0a86388434619d4d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040a4719dbfc0359030e6a8e3c10140472908a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VelorettiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
